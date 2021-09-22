ea7f6a2f6cff22d9477e309413b2c57ffa4e1bf8
David de Ingunza <deingun@gmail.com>
2021-09-22 09:09:24 +0200

    transaction DTO

--- src/main/java/com/deingun/bankingsystem/controller/dto/TransactionDTO.java
+++ src/main/java/com/deingun/bankingsystem/controller/dto/TransactionDTO.java
@@ -1,24 +1,13 @@
 package com.deingun.bankingsystem.controller.dto;
 
-import com.deingun.bankingsystem.model.account.Account;
-import com.deingun.bankingsystem.model.user.User;
-import com.deingun.bankingsystem.utils.Money;
-
-import javax.persistence.*;
-import javax.validation.constraints.NotEmpty;
 
 public class TransactionDTO {
 
-    @NotEmpty(message = "Origin Account must be provided")
     private String originAccount;
-    @NotEmpty(message = "Destination Account must be provided")
     private String destinationAccount;
-
     private String accountNumber;
-    @NotEmpty(message = "Amount must be provided")
     private String amount;
     private String hashedKey;
-
     private String secretKey;
     private String transactionType;
 
@@ -80,3 +69,4 @@ public class TransactionDTO {
         this.secretKey = secretKey;
     }
 }
+
